ebc988bcf4ae51790791c185d6f1a6b28b8d56f6
DumboJ <dumbojxx@gmail.com>
2023-03-09 23:58:56 +0800

    sync.Mutex lock/unlock resolve concurrent add wrong

--- src/lagou/routinue/routinue-add.go
+++ src/lagou/routinue/routinue-add.go
@@ -1,17 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-var sum = 0
+var (
+	sum   = 0
+	mutex sync.Mutex
+)
 
 // concurrent wrong
+//resolve : add sync.Mutex
 func main() {
 	for i := 0; i < 100; i++ {
 		go add(10)
 	}
+	//防止程序提前退出
+	time.Sleep(2 * time.Second)
 	fmt.Println(sum) //990
 }
 
 func add(i int) {
+	mutex.Lock()
+	defer mutex.Unlock()
 	sum += i
 }
