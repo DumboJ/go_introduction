a18adfd44a46a2a86c4184086c1d35d2536857c3
DumboJ <dumbojxx@gmail.com>
2023-03-21 23:44:04 +0800

    channel fibonacci demo

---
+++ src/offical/routine/range-and-close.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//使用channel实现一个斐波那契函数
+func fibonacci(len int, ch chan int) {
+	a, b := 0, 1
+	for i := 0; i < len; i++ {
+		ch <- a
+		a, b = b, a+b
+	}
+	//only  the sender can close channel ,receiver can't close it
+	close(ch)
+}
+func main() {
+	ch := make(chan int, 10)
+	//attention ： cap() is the size of defined channel ,instantiation  channel invoke len() return 0
+	go fibonacci(cap(ch), ch)
+	for n := range ch {
+		fmt.Println(n)
+	}
+}
